7fa6f7c08314358baf347f3fa6353d26dfc18ba2
Sainik Khaddar <73096052+sainik-khaddar@users.noreply.github.com>
2022-07-24 04:17:24 +0530

    Time: 28 ms (75.63%), Space: 18 MB (73.44%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
+    bool isvalid(int row, int col,vector<vector<char>>& board,int val )
+    {
+        for(int i=0;i<9;i++)
+        {
+            if(board[row][i]-'0'==val)
+                return false;
+        }
+        for(int i=0;i<9;i++)
+        {
+            if(board[i][col]-'0'==val)
+                return false;
+        }
+        for(int i=0;i<9;i++)
+        {
+            if(board[3*(row/3) +(i/3)][3*(col/3) +(i%3)]-'0'==val)
+                return false;
+        }
+        return true;
+    }
     bool isValidSudoku(vector<vector<char>>& board) {
-        bool row[9][9]={false};
-        bool col[9][9]={false};
-        bool box[9][9]={false};
-        
         for(int i=0;i<9;i++)
         {
             for(int j=0;j<9;j++)
             {
                 if(board[i][j]!='.')
                 {
-                    int curr= board[i][j]-'0'-1;
-                    int idx=i/3*3 +j/3;
-                    if(row[i][curr] || col[j][curr] || box[idx][curr])
+                    int x=board[i][j]-'0';
+                    board[i][j]='.';
+                    if(isvalid(i,j,board,x))
+                    {
+                        board[i][j]=x;
+                    }
+                    else
+                    {
                         return false;
-                    row[i][curr] = col[j][curr] = box[idx][curr]= true;
+                    }
                 }
             }
         }
