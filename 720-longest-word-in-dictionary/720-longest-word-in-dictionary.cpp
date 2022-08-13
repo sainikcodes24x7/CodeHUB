2cb0b4af3011d8678efcffc3b506636ab319925d
Sainik Khaddar <73096052+sainik-khaddar@users.noreply.github.com>
2022-08-13 19:43:55 +0530

    Time: 140 ms (27.49%), Space: 30.6 MB (49.25%) - LeetHub

---
+++ 720-longest-word-in-dictionary/720-longest-word-in-dictionary.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isvalid(string word, unordered_set<string>&st, int &cnt)
+    {
+        if(word.size()==1 and st.find(word)!=st.end())
+            return true;
+        word.pop_back();
+        if(st.find(word)!=st.end())
+        {
+            cnt++;
+            return isvalid(word,st,cnt);
+        }
+        return false;
+    }
+    string longestWord(vector<string>& words) {
+        int n=words.size();
+        unordered_set<string>st(words.begin(),words.end());
+        int maxcnt=-1;
+        string str="";
+        sort(words.begin(),words.end());
+        for(int i=0;i<n;i++)
+        {
+            int cnt=0;
+            if(isvalid(words[i],st,cnt) and cnt>maxcnt)
+            {
+                maxcnt=cnt;
+                str=words[i];
+            }
+        }
+        return str;
+    }
+};
