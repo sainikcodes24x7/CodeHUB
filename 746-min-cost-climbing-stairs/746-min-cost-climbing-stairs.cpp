63c8e6f0b2c4140688e125740ee3c240c5520c4b
Sainik Khaddar <73096052+sainik-khaddar@users.noreply.github.com>
2022-08-18 23:47:50 +0530

    Time: 17 ms (17.98%), Space: 13.7 MB (48.96%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -11,7 +11,7 @@ public:
         int n=cost.size();
         for(int i=2;i<n;i++)
         {
-            cost[i]=cost[i]+min(cost[i-2],cost[i-1]);
+            cost[i]+=min(cost[i-2],cost[i-1]);
         }
         return min(cost[n-1],cost[n-2]);
     }
