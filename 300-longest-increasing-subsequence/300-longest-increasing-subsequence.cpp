14a805f4196bd7cb8cf4d60909615df71d80282d
Sainik Khaddar <73096052+sainik-khaddar@users.noreply.github.com>
2022-08-19 17:21:05 +0530

    Time: 841 ms (15.74%), Space: 292.3 MB (8.08%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,25 +1,62 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>curRow(n+1,0);
-        vector<int>nextrow(n+1,0);
+    /*
+    int solvememo(vector<int>& nums, int n, int curr, int prev, vector<vector<int>>dp)
+    {
+        if(curr==n)
+            return 0;
+        if(dp[curr][prev+1]!=-1)
+            return dp[curr][prev+1];
+        int take=0;
         
+        // when she is taken
+        if(prev==-1 || nums[curr]>nums[prev])
+        {
+            take=1+solvememo(nums,n,curr+1,curr,dp);
+        }
+        
+        //when she is not taken
+        int nottaken=0+solvememo(nums,n,curr+1,prev,dp);
+        
+        //that's what she said
+        dp[curr][prev+1]=max(take,nottaken);
+        return dp[curr][prev+1];
+    }
+    */
+     int solvedp(vector<int>& nums, int n)
+    {
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
         for(int curr=n-1;curr>=0;curr--)
         {
-            for(int prev=curr-1;prev>=-1;prev--){
-                
-                //take
+            for(int prev=curr-1;prev>=-1;prev--)
+            {
                 int take=0;
-                if(prev==-1 || nums[curr]>nums[prev])
-                    take=1+nextrow[curr+1];
-                
-                //exclude
-                int nottake=0+nextrow[prev+1];
-                curRow[prev+1]=max(take,nottake);
-            }
-            nextrow=curRow;
+        
+                // when she is taken
+                    if(prev==-1 || nums[curr]>nums[prev])
+        {
+            take=1+dp[curr+1][curr+1];
         }
-        return nextrow[0];
+        
+        //when she is not taken
+        int nottaken=0+dp[curr+1][prev+1];
+                dp[curr][prev+1]=max(take,nottaken);
+            }
+        }
+        
+        
+        //that's what she said
+        
+        return dp[0][0];
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        // for memorization
+        /*
+       vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return solvememo(nums,n,0,-1,dp);
+        */
+        
+        return solvedp(nums,n);
     }
 };
