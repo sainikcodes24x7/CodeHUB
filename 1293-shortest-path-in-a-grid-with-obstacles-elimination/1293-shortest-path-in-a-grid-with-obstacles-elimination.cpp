d538111cd45ee8a123250571b42e84487569881e
Sainik Khaddar <73096052+sainik-khaddar@users.noreply.github.com>
2022-10-31 00:51:13 +0530

    Time: 748 ms (16.37%), Space: 29.8 MB (56.47%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,33 +1,46 @@
 class Solution {
 public:
-    int shortestPath(vector<vector<int>>& grid, int k) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> visited(m, vector<int>(n,-1));
+       int dp[47][47][47*47];
+    int helper1(vector<vector<int>>& grid, int k, int r, int c, vector<vector<int>> &vis)
+    {
+        if(r<0 || r>=grid.size() || c<0 || c>=grid[0].size() || vis[r][c])return 1e6;
+        if(dp[r][c][k] != -1)return dp[r][c][k];
+        if(r==0 && c==0)return dp[r][c][k] = 0;
         
-        queue<vector<int>> qu;
-        qu.push({0, 0, 0, k});
-        while(!qu.empty()){
-            
-            auto t = qu.front();
-            qu.pop();
-            int x = t[0], y = t[1];
-            if(x<0 || y<0 || x>=m || y>=n) continue;
-            if(x==m-1 && y==n-1) return t[2];
-            // obstacle
-            if(grid[x][y] == 1){
-                if(t[3] >0) t[3]--; // If we have k remainaning
-                else continue;      // Else stop this way
+        
+        
+        if(grid[r][c]==1)
+        {
+            if(k>0)
+            {
+                k--;
             }
-            
-            if(visited[x][y]!=-1 && visited[x][y] >= t[3]) continue; // Check if we have less k for this path, pass if yes
-            
-            visited[x][y] = t[3];
-            qu.push({x+1,y,t[2]+1, t[3]});
-            qu.push({x-1,y,t[2]+1, t[3]});
-            qu.push({x,y+1,t[2]+1, t[3]});
-            qu.push({x,y-1,t[2]+1, t[3]});
+            else{
+                return dp[r][c][k] = 1e6;
+            }
         }
-        return -1;
+        
+        
+        
+        vis[r][c]=1;
+        
+        int w1, w2, w3, w4;
+        w1=w2=w3=w4=1e6;
+        w1 = helper1(grid,k,r-1,c,vis);
+        w2 = helper1(grid,k,r,c-1,vis);
+        w3 = helper1(grid,k,r+1,c,vis);
+        w4 = helper1(grid,k,r,c+1,vis);
+        
+        vis[r][c]=0;
+        
+        return dp[r][c][k] = 1+min({w1, w2, w3, w4});
+    }
+    
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        memset(dp, -1, sizeof dp);
+        vector<vector<int>> vis1((int)grid.size(), vector<int>((int)grid[0].size(), 0));
+        int ans2 = helper1(grid, k, grid.size()-1, grid[0].size()-1, vis1);
+        if(ans2>=1e6)return -1;
+        return ans2;
     }
 };
