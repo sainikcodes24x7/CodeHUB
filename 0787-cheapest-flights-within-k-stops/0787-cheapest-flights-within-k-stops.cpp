df74a8075f17f8171110c58a90f6d06d3452c3fb
Sainik Khaddar <73096052+sainik-khaddar@users.noreply.github.com>
2022-11-05 04:21:40 +0530

    Time: 49 ms (65.15%), Space: 13.5 MB (61.27%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        unordered_map<int,list<pair<int,int>>>mp;
+        for(int i=0;i<flights.size();i++)
+        {
+            int from=flights[i][0];
+            int to=flights[i][1];
+            int price=flights[i][2];
+            mp[from].push_back({price,to});
+        }
+        vector<int>cost(n,1e9);
+        
+        queue<pair<int,pair<int,int>>>q;
+        
+        q.push({0,{0,src}});
+        cost[src]=0;
+        
+        while(!q.empty())
+        {
+            int kval=q.front().first;
+            int price=q.front().second.first;
+            int currcity=q.front().second.second;
+            q.pop();
+            
+            if(kval<=k)
+            {
+                for(auto nbr:mp[currcity])
+                {
+                    if(price+nbr.first<cost[nbr.second])
+                    {
+                        cost[nbr.second]=price+nbr.first;
+                        q.push({kval+1,{cost[nbr.second],nbr.second}});
+                    }
+                }
+            }
+        }
+        return cost[dst]==1e9?-1:cost[dst];
+    }
+};
