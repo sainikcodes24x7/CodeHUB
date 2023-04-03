721ae7bd76d134f78050ae6cfff61d220f0fb4ae
Sainik Khaddar <73096052+sainik-khaddar@users.noreply.github.com>
2023-04-03 13:41:02 +0530

    Time: 87 ms (66.67%), Space: 42 MB (59.84%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int i=0,j=people.size()-1,cnt=0;
+        while(i<=j)
+        {
+            if(people[i]+people[j]<=limit)
+            {
+                i++;
+                j--;
+                cnt++;
+            }
+            else
+            {
+                j--;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
