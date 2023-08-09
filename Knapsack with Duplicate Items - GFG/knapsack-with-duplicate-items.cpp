df24146dda9eb43526a66956de4c4dfc7de8b795
Sainik Khaddar <73096052+playboySeductor@users.noreply.github.com>
2023-08-09 19:32:35 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -9,33 +9,26 @@ using namespace std;
 
 class Solution{
 public:
-    int knapSack(int N, int W, int val[], int wt[])
+    int knapSack(int n, int w, int val[], int wt[])
     {
         // code here
-        int dp[N+1][W+1];
-        for(int i=0;i<=N;i++)
-        {
+        vector<vector<int>>dp(n+1,vector<int>(w+1,0));
+        //int dp[n+1][w+1];
+        for(int i=0;i<=n;i++){
             dp[i][0]=0;
         }
-        for(int i=0;i<=W;i++)
-        {
+        for(int i=0;i<=w;i++){
             dp[0][i]=0;
         }
-        for(int i=1;i<=N;i++)
-        {
-            for(int j=1;j<=W;j++)
-            {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=w;j++){
                 if(wt[i-1]<=j)
-                {
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-wt[i-1]]+val[i-1]);
-                }
+                dp[i][j]=max(dp[i-1][j],dp[i][j-wt[i-1]]+val[i-1]);
                 else
-                {
-                    dp[i][j]=dp[i-1][j];
-                }
+                dp[i][j]=dp[i-1][j];
             }
         }
-        return dp[N][W];
+        return dp[n][w];
     }
 };
 
